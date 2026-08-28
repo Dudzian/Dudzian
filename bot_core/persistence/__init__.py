9dd0cc001f5820d5f800cace6e32bbb2d699339f
Dudzian <kamil.dudzinski.1988@gmail.com>
2026-08-28 14:56:41 +0200

    feat(persistence): add S2A record validation kernel

--- bot_core/persistence/__init__.py
+++ bot_core/persistence/__init__.py
@@ -1,5 +1,13 @@
 """Produkcyjne fundamenty trwałego StateStore."""
 
+from .records import PersistenceRecord, PersistenceRecordError, validate_persistence_record
 from .state_store import StateStoreError, StateStoreMetadata, SQLiteStateStore
 
-__all__ = ["SQLiteStateStore", "StateStoreError", "StateStoreMetadata"]
+__all__ = [
+    "PersistenceRecord",
+    "PersistenceRecordError",
+    "SQLiteStateStore",
+    "StateStoreError",
+    "StateStoreMetadata",
+    "validate_persistence_record",
+]
